872bfd007737387118834fdb1e46bdb4342ad4a0
sahilali13 <ali.sahil.a743@gmail.com>
2024-05-09 22:13:10 +0530

    completed Simple_Shop

--- Simple_Shop/src/components/Header.jsx
+++ Simple_Shop/src/components/Header.jsx
@@ -1,45 +1,51 @@
-import { useRef } from 'react';
+import { useRef, useContext } from 'react';
 
 import CartModal from './CartModal.jsx';
+import { CartContext } from '../store/shopping-cart-context.jsx';
 
 export default function Header({ cart, onUpdateCartItemQuantity }) {
-  const modal = useRef();
+	const modal = useRef();
 
-  const cartQuantity = cart.items.length;
+	const { items } = useContext(CartContext);
 
-  function handleOpenCartClick() {
-    modal.current.open();
-  }
+	const cartQuantity = items.length;
 
-  let modalActions = <button>Close</button>;
+	function handleOpenCartClick() {
+		modal.current.open();
+	}
 
-  if (cartQuantity > 0) {
-    modalActions = (
-      <>
-        <button>Close</button>
-        <button>Checkout</button>
-      </>
-    );
-  }
+	let modalActions = <button>Close</button>;
 
-  return (
-    <>
-      <CartModal
-        ref={modal}
-        cartItems={cart.items}
-        onUpdateCartItemQuantity={onUpdateCartItemQuantity}
-        title="Your Cart"
-        actions={modalActions}
-      />
-      <header id="main-header">
-        <div id="main-title">
-          <img src="logo.png" alt="Elegant model" />
-          <h1>Elegant Context</h1>
-        </div>
-        <p>
-          <button onClick={handleOpenCartClick}>Cart ({cartQuantity})</button>
-        </p>
-      </header>
-    </>
-  );
+	if (cartQuantity > 0) {
+		modalActions = (
+			<>
+				<button>Close</button>
+				<button>Checkout</button>
+			</>
+		);
+	}
+
+	return (
+		<>
+			<CartModal
+				ref={modal}
+				title='Your Cart'
+				actions={modalActions}
+			/>
+			<header id='main-header'>
+				<div id='main-title'>
+					<img
+						src='logo.png'
+						alt='Elegant model'
+					/>
+					<h1>Elegant Context</h1>
+				</div>
+				<p>
+					<button onClick={handleOpenCartClick}>
+						Cart ({cartQuantity})
+					</button>
+				</p>
+			</header>
+		</>
+	);
 }
